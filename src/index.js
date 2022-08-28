2b9da4e9b07daae74551d6061f4aa95a1aa73e02
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-08-28 16:41:48 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
+require('dotenv').config()
+require('./lib/db')
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-require('dotenv').config()
 
 const userRoute = require('./routes/user')
 
 // settings
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3333
 
 // middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/api', userRoute)
 
 // routes
-app.get('/', (req, res) => {
-  res.send(200, { menssage: 'Bienvenido a mi Api REST' })
+app.get("/", async (req, res) => {
+  res.send(200, { message: "Please visit /api/users to view all users" })
 })
 
-// conexion a la base de datos mongodB
-mongoose.connect(process.env.MONGODB_KEY)
-  .then(() => console.log('Conectado a la base de datos'))
-  .catch(err => console.log(err))
+app.use('/api', userRoute)
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
